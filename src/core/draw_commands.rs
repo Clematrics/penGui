365aa1500ec897274918d791f929af472bff5ebb
Emile Trotignon <emile.trotignon@gmail.com>
2020-10-17 13:49:07 +0200

    structure

--- src/core/draw_commands.rs
+++ src/core/draw_commands.rs
@@ -1,25 +1,25 @@
-struct Vertex {
-	position : [f32; 3],
-	color : [f32; 4]
+pub struct Vertex {
+	position: [f32; 3],
+	color: [f32; 4],
 }
 
-struct Uniform {} // TODO : adapt definition to real uniforms
+pub struct Uniform {} // TODO : adapt definition to real uniforms
 
-enum DrawMode {
+pub enum DrawMode {
 	TriangleFan,
 	LineFan,
 	// ... TODO : to complete
 }
 
-struct DrawCommand<Texture> {
-	vertex_buffer : Vec<Vertex>,
-	index_buffer  : Vec<u32>, // Wrapper
-	draw_mode     : DrawMode, //
-	clipping      : [[f32;2];2],
-	uniforms      : Vec<Uniform> // Option
-	texture       : Texture
+pub struct DrawCommand<Texture> {
+	pub vertex_buffer: Vec<Vertex>,
+	pub index_buffer: Vec<u32>, // Wrapper
+	pub draw_mode: DrawMode,    //
+	pub clipping: [[f32; 2]; 2],
+	pub uniforms: Vec<Uniform>, // Option
+	pub texture: Option<Texture>,
 }
 
-trait Backend<T> {
-	fn draw_command(&self, T, &DrawCommand);
+pub trait Backend<T> {
+	fn draw_command(&self, t: T, d: &DrawCommand<T>);
 }
---
+++ src/core/mod.rs
@@ -0,0 +1,4 @@
+pub mod context;
+pub mod draw_commands;
+pub mod event;
+pub mod widget;
