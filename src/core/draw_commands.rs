a796499e224eb932cf8af76a1ce77fb3df7934a4
Emile Trotignon <emile.trotignon@gmail.com>
2020-10-19 10:53:44 +0200

    remove texture generics

--- src/core/draw_commands.rs
+++ src/core/draw_commands.rs
@@ -11,15 +11,15 @@ pub enum DrawMode {
 	// ... TODO : to complete
 }
 
-pub struct DrawCommand<Texture = u32> {
+pub struct DrawCommand {
 	pub vertex_buffer: Vec<Vertex>,
 	pub index_buffer: Vec<u32>, // Wrapper
 	pub draw_mode: DrawMode,    //
 	pub clipping: [[f32; 2]; 2],
 	pub uniforms: Vec<Uniform>, // Option
-	pub texture: Option<Texture>,
+	pub texture: Option<u32>,
 }
 
-pub trait Backend<T = u32> {
-	fn draw_command(&self, t: T, d: &DrawCommand<T>);
+pub trait Backend {
+	fn draw_command(&self, t: Option<u32>, d: &DrawCommand);
 }
