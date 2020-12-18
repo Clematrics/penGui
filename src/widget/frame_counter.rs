750a3d48988937b24ad02c4f95ee952543e27317
Emile Trotignon <emile.trotignon@gmail.com>
2020-12-18 15:57:35 +0100

    frame counter widget

---
+++ src/widget/frame_counter.rs
@@ -0,0 +1,80 @@
+use nalgebra::Point3;
+
+use crate::core::{
+    CodeLocation, ComponentId, DrawCommand, DrawList, DrawMode, NodeMetadata, NodeReference,
+    TextureId, Uniforms, Vertex, WidgetBuilder, WidgetLogic,
+};
+
+pub struct FrameCounter {
+    count: u32,
+}
+
+impl FrameCounter {
+    pub fn new() -> Self {
+        Self { count: 0 }
+    }
+
+    /*pub fn position(mut self, pos: (f32, f32, f32)) -> Self {
+        self.position = pos;
+        self
+    }*/
+}
+
+impl WidgetBuilder for FrameCounter {
+    type AchievedType = FrameCounter;
+    type BuildFeedback = u32;
+
+    fn update(self, _metadata: &NodeMetadata, old: &mut Self::AchievedType) {
+        old.count = self.count + 1
+    }
+
+    fn create(self) -> Self::AchievedType {
+        self
+    }
+
+    fn build(self, loc: CodeLocation, parent: NodeReference) -> Self::BuildFeedback {
+        let id = ComponentId::new::<Self::AchievedType>(loc);
+        let r = self.count;
+        parent
+            .borrow_mut()
+            .query::<Self::AchievedType>(id)
+            .update(self);
+
+        r
+    }
+}
+
+impl WidgetLogic for FrameCounter {}
+
+#[cfg(test)]
+mod tests {
+
+    use crate::core::*;
+    use crate::*;
+    use widget::*;
+
+    fn test_frame_counter() {
+        let mut ui = Interface::new();
+        for i in 0..12 {
+            ui.new_frame();
+            WindowBuilder::new(move |ui| {
+                PaddingBuilder::new((0.2, 0.2), Button::new("label not displayed".to_string()))
+                    .build(loc!(), ui.clone());
+                Button::new("label not displayed".to_string()).build(loc!(), ui.clone());
+
+                assert_eq!(i, FrameCounter::new().build(loc!(), ui.clone()));
+                assert_eq!(i, FrameCounter::new().build(loc!(), ui.clone()));
+
+
+                Button::new("label not displayed".to_string())
+                    .color((1., 0., 0., 0.5))
+                    .color((1., 1., 1., 1.))
+                    .build(loc!(), ui.clone());
+            })
+            .build(loc!(), ui.root.clone());
+
+            ui.end_frame();
+            ui.generate_layout();
+        }
+    }
+}
