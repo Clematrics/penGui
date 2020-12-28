ad931e61f1fb0778c9e3572d644f9f09c4662338
Clematrics <clematrics.studio@gmail.com>
2020-12-28 17:47:35 +0100

    Text widget: the full string is displayed

--- src/widget/text.rs
+++ src/widget/text.rs
@@ -79,36 +79,65 @@ impl WidgetLogic for Text {
             .expect("A font is not owned anymore by the backend");
         uniforms.texture = Some(font.borrow().get_texture());
 
-        let CharacterInfo {
-            texture_uv: (u, v),
-            texture_size: (w, h),
-            ..
-        } = font.borrow_mut().char_info('c', None);
+        let count = self.text.chars().count();
+        let mut vertex_buffer = Vec::with_capacity(count * 4);
+        let mut index_buffer = Vec::<u32>::with_capacity(count * 6);
+        let mut cursor = 0.;
+        let mut last_char = None;
+        self.text.chars().enumerate().for_each(|(i, c)| {
+            let mut font = font.borrow_mut();
+
+            let base = 4 * i;
+
+            let CharacterInfo {
+                texture_uv: (u, v),
+                texture_size: (w, h),
+                advance_width,
+                top_left: (tx, ty),
+                bottom_right: (bx, by),
+                kerning,
+            } = font.char_info(c, last_char);
+
+            let ax = cursor + tx + kerning;
+            let ay = ty;
+            let bx = cursor + bx + kerning;
+            let by = by;
+
+            vertex_buffer.push(Vertex {
+                position: [ax, ay, 0.],
+                color,
+                tex_uv: [u, v],
+            });
+            vertex_buffer.push(Vertex {
+                position: [bx, ay, 0.],
+                color,
+                tex_uv: [u + w, v],
+            });
+            vertex_buffer.push(Vertex {
+                position: [ax, by, 0.],
+                color,
+                tex_uv: [u, v + h],
+            });
+            vertex_buffer.push(Vertex {
+                position: [bx, by, 0.],
+                color,
+                tex_uv: [u + w, v + h],
+            });
+
+            index_buffer.push(base as u32);
+            index_buffer.push((base + 1) as u32);
+            index_buffer.push((base + 2) as u32);
+            index_buffer.push((base + 1) as u32);
+            index_buffer.push((base + 2) as u32);
+            index_buffer.push((base + 3) as u32);
+
+            cursor += advance_width;
+            last_char = Some(c);
+        });
 
         let command = DrawCommand {
-            vertex_buffer: vec![
-                Vertex {
-                    position: [-size.0 / 2., -size.1 / 2., 0.],
-                    color,
-                    tex_uv: [u, v],
-                },
-                Vertex {
-                    position: [size.0 / 2., -size.1 / 2., 0.],
-                    color,
-                    tex_uv: [u + w, v],
-                },
-                Vertex {
-                    position: [-size.0 / 2., size.1 / 2., 0.],
-                    color,
-                    tex_uv: [u, v + h],
-                },
-                Vertex {
-                    position: [size.0 / 2., size.1 / 2., 0.],
-                    color,
-                    tex_uv: [u + w, v + h],
-                },
-            ],
-            index_buffer: vec![0, 1, 2, 1, 2, 3],
+            vertex_buffer,
+            index_buffer,
             draw_mode: DrawMode::Triangles,
             uniforms,
         };
