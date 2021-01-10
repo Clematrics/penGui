7f07b22b1e7c62e84150ebe591bc8805cc9aa61e
Clematrics <clematrics.studio@gmail.com>
2021-01-10 23:23:31 +0100

    Documented font.rs

--- src/core/font.rs
+++ src/core/font.rs
@@ -1,5 +1,7 @@
 use crate::core::TextureId;
 
+/// Holds all information necessary to draw a character
+/// or to understand its shape.
 pub struct CharacterInfo {
     pub texture_uv: (f32, f32),
     pub texture_size: (f32, f32),
@@ -9,15 +11,36 @@ pub struct CharacterInfo {
     pub kerning: f32,
 }
 
+/// The vertical metrics of a font.
+///
+/// `ascent` is the maximum height a character can take
+/// from the baseline (positive number in general)
+///
+/// `descent` is the maximum depth a character can have
+/// starting at the baseline (typically negative)
+///
+/// `line_gap` is the space between two lines
 pub struct VerticalMetrics {
     pub ascent: f32,
     pub descent: f32,
     pub line_gap: f32,
 }
 
+/// A trait describing an atlas of characters tied to a font.
 pub trait FontAtlas {
+    /// Returns the vertical metrics of the font behind this atlas
     fn get_vertical_metrics(&self) -> VerticalMetrics;
+
+    /// Returns the texture ID having all necessary characters
     fn get_texture(&self) -> TextureId;
+
+    /// Returns the character information.
+    ///
+    /// The previous character can be given to get more accurate information,
+    /// especially for the kerning between the two characters
     fn char_info(&mut self, character: char, previous_char: Option<char>) -> CharacterInfo;
+
+    /// Special function which returns the width and height a single-line
+    /// string would take if rendered with this atlas.
     fn size_of(&self, string: &str) -> (f32, f32);
 }
